e97c3ee336365587993474f2d8eaecfe460f1df4
sungsikyang92 <sungsik.yang92@gmail.com>
2000-08-05 23:01:32 +0000

    TOSS

---
+++ PROGRAMMERS/타켓넘버.py
@@ -0,0 +1,13 @@
+numbers = [1, 1, 1, 1, 1]
+target = 3
+# numbers = [4, 1, 2, 1]
+# target = 4
+
+
+def solution(numbers, target):
+    answer = 0
+    numbers_len = len(numbers)
+
+    return answer
+
+print(solution(numbers,target))
