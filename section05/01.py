02848bbeb37d15c21d25665384b1f56a870d294d
sungsikyang92 <sungsik.yang92@gmail.com>
2022-02-09 16:56:25 +0900

    section5

--- section05/01.py
+++ section05/01.py
@@ -1,9 +1,15 @@
 # 가장 큰 수
-
+# 어케 풀까..? 반복문으로 리스트에 집어넣어준다.
+# 넣어줌에 있어서, 넣는것이 앞에 넣어져있는것보다 크면 뺀다(제외)...?
 num, m = map(str, input().split())
-stack = []
+m = int(m)
+stk = []
 for i in num:
-    while stack and m > 0 and stack[-1] < i:
-        stack.pop()
+    while stk and stk[-1] < i and m > 0:
+        stk.pop()
         m -= 1
-    stack.append(i)
+    stk.append(i)
+# print(m)
+if m != 0:
+    stk = stk[:-m]
+print(''.join(stk))
