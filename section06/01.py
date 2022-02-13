90862c6ba8fa650dbffa34176097173488693673
sungsikyang92 <sungsik.yang92@gmail.com>
2022-02-13 23:43:35 +0900

    section6 01 재귀함수란

---
+++ section06/01.py
@@ -0,0 +1,28 @@
+# 재귀함수를 이용한 이진수 출력
+
+n = int(input())
+
+result = 0
+cnt = 0
+def make_binary_num(n):
+    global result
+    global cnt
+    if n > 1:
+        result += (n % 2) * (10 ** cnt)
+        cnt += 1
+        make_binary_num(n // 2)
+    else:
+        result += (n % 2) * (10 ** cnt)
+        cnt += 1
+        result += (n // 2) * (10 ** cnt)
+    return result
+
+print(make_binary_num(n))
+
+# def make_binary_num(n):
+#     if n == 0:
+#         return
+#     else:
+#         make_binary_num(n // 2)
+#         print(n % 2, end = ' ')
+# make_binary_num(n)
