1c2e91cf88bc4fd6a37684a138f2c0b35a5e0ec9
sungsikyang92 <sungsik.yang92@gmail.com>
2022-02-23 16:18:27 +0900

    송아지 찾기

--- section07/07.py
+++ section07/07.py
@@ -1,2 +1,23 @@
 # 송아지 찾기
 
+from collections import deque
+MAX = 10000
+chk = [0] * (MAX + 1)
+dis = [0] * (MAX + 1)
+s, e = map(int, input().split())
+chk[s] = 1
+dis[s] = 0
+dq = deque()
+dq.append(s)
+
+while dq:
+    current = dq.popleft()
+    if current == e:
+        break
+    for next in (current - 1, current + 1, current + 5):
+        if 0 <= next <= MAX:
+            if chk[next] == 0:
+                dq.append(next)
+                chk[next] = 1
+                dis[next] = dis[current] + 1
+print(dis[e])
