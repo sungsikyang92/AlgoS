3530691cd054dbdd9b9e699811d369e4df7655a1
sungsikyang92 <sungsik.yang92@gmail.com>
2022-07-01 01:59:12 +0900

    ARRAY

--- BAEKJOON/ARRAY/4344_평균은넘겠지.py
+++ BAEKJOON/ARRAY/4344_평균은넘겠지.py
@@ -1,2 +1,14 @@
 n = int(input())
 
+for x in range(n):
+    tmp = list(map(int, input().split()))
+    tmp_sum = 0
+    for i in range(1, tmp[0] + 1):
+        tmp_sum += tmp[i]
+    avg = tmp_sum/tmp[0]
+    cnt = 0
+    for y in range(1, tmp[0] + 1):
+        if tmp[y] > avg:
+            cnt += 1
+    result = cnt/tmp[0]*100
+    print("{0:.3f}{1}".format(result, "%"))
