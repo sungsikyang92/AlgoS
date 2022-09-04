6b7e90c1ae97bd45b345b62a39a12dc6c1da1ec4
sungsikyang92 <sungsik.yang92@gmail.com>
2022-09-04 17:17:15 +0900

    push

--- PROGRAMMERS/최소직사각형.py
+++ PROGRAMMERS/최소직사각형.py
@@ -1,20 +1,15 @@
 # sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
 sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
 def solution(sizes):
-    answer = 0
-
-    tmp = sorted(sizes, key = lambda x : (-x[0], x[1]))
-    print(tmp)
-    print(tmp[0])
-    print(tmp[-1][0])
-    print(tmp[-1][1])
-    tmp_1 = tmp[-1][0]
-    tmp[-1][0] = tmp[-1][1]
-    tmp[-1][0] = tmp[-1][1]
-    tmp[-1][1] = tmp_1
-    tmp_2 = sorted(tmp, key=lambda x: (-x[0], x[1]))
-    # print(tmp_2)
-    answer = tmp_2[0][0] * tmp_2[-1][1]
-    return answer
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+    return max(w) * max(h)
 
 print(solution(sizes))
