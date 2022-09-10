a0ca4d1a72620ed3a059e9a07f17d02a14d59c75
sungsikyang92 <sungsik.yang92@gmail.com>
2022-09-10 21:23:52 +0900

    push

--- PROGRAMMERS/소수만들기.py
+++ PROGRAMMERS/소수만들기.py
@@ -1,1 +1,28 @@
-ㅇ
+# nums = [1,2,3,4]
+nums = [1,2,7,6,4]
+
+def is_prime(x):
+    if x < 2:
+        return False
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
+    return True
+
+def solution(nums):
+    answer = -1
+    cnt = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            num = 0
+            for k in range(j+1, len(nums)):
+                num = nums[i] + nums[j] + nums[k]
+                if is_prime(num):
+                    cnt += 1
+
+    answer = cnt
+    return answer
+
+print(solution(nums))
