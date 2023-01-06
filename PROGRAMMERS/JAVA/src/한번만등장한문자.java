816b84172466c2a703402a8b192ac60c937d7fdf
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-07 02:20:01 +0900

    PG JAVA PRAC

--- PROGRAMMERS/JAVA/src/한번만등장한문자.java
+++ PROGRAMMERS/JAVA/src/한번만등장한문자.java
@@ -1,11 +1,20 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class 한번만등장한문자 {
-    public String solution(String s) {
+    public static String solution(String s) {
         String answer = "";
-        List<String> s_list = Arrays.asList(s.split(""));
-
+        int[] alphabet_list = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alphabet_list[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < alphabet_list.length; i++) {
+            if (alphabet_list[i] == 1) {
+                answer += (char) (i + 'a');
+            }
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String result = solution("hello");
+        System.out.println(result);
     }
 }
