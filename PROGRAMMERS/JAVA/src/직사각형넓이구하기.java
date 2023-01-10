35b83b86804f95c7fe1b8b82a111157e7dd1e027
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-10 17:03:31 +0900

    PG lv.0 JAVA PRAC

--- PROGRAMMERS/JAVA/src/직사각형넓이구하기.java
+++ PROGRAMMERS/JAVA/src/직사각형넓이구하기.java
@@ -1,7 +1,16 @@
 public class 직사각형넓이구하기 {
     public int solution(int[][] dots) {
         int answer = 0;
-
+        int vertical = 0;
+        int horizontal = 0;
+        for (int i = 1; i < dots.length; i++) {
+                if (dots[0][1] == dots[i][1] && dots[0][0] != dots[i][0]) {
+                    vertical = dots[0][0] - dots[i][0];
+                } else if (dots[0][0] == dots[i][0] && dots[0][1] != dots[i][1]) {
+                    horizontal = dots[0][1] - dots[i][1];
+                }
+        }
+        answer = Math.abs(vertical) * Math.abs(horizontal);
         return answer;
     }
 }
