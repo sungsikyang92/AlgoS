8f56197c0d672390f283abdda063a698d6c936e4
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-11 22:49:10 +0900

    PG lv.0 JAVA PRAC

---
+++ PROGRAMMERS/JAVA/src/유한소수판별하기.java
@@ -0,0 +1,2 @@
+public class 유한소수판별하기 {
+}
---
+++ PROGRAMMERS/JAVA/src/치킨쿠폰.java
@@ -0,0 +1,20 @@
+public class 치킨쿠폰 {
+    public static int solution(int chicken) {
+        int answer = 0;
+        int cpn = 0;
+        int chic_count = 0;
+        while (chicken >= 10) {
+            chic_count = chicken / 10;
+            cpn = chicken % 10;
+            chicken = chic_count + cpn;
+            answer += chic_count;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int result = solution(1999);
+        System.out.println(result);
+    }
+}
