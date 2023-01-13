a74090c494d2db20b3c348a83e8d8c76406e5879
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-13 20:02:42 +0900

    PG lv.0 JAVA PRAC

--- PROGRAMMERS/JAVA/src/유한소수판별하기.java
+++ PROGRAMMERS/JAVA/src/유한소수판별하기.java
@@ -1,2 +1,29 @@
 public class 유한소수판별하기 {
+    public static int GCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return GCD(b, a % b);
+    }
+    public static int solution(int a, int b) {
+        int gcd_num = GCD(a, b);
+        int btm = b / gcd_num;
+        while (btm != 1) {
+            if (btm % 2 == 0) {
+                btm /= 2;
+            } else if (btm % 5 == 0) {
+                btm /= 5;
+            } else {
+                return 2;
+            }
+        }
+
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int result = solution(7, 20);
+        System.out.println(result);
+    }
 }
---
+++ PROGRAMMERS/JAVA/src/특이한정렬.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 특이한정렬 {
+    public static int[] solution(int[] numlist, int n) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numlist = new int[]{1, 2, 3, 4, 5, 6};
+        int[] result = solution(numlist, 4);
+        System.out.println(result);
+    }
+}
