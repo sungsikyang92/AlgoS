2626528c5afbe79acb41cb9d7b1d9fc6b659ac96
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-15 16:01:52 +0900

    PG lv.0 JAVA PRAC

--- PROGRAMMERS/JAVA/src/신을모시는사당.java
+++ PROGRAMMERS/JAVA/src/신을모시는사당.java
@@ -6,44 +6,49 @@ import java.util.stream.Stream;
 public class 신을모시는사당 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N;
-        N = sc.nextInt();
+        int N = sc.nextInt();
         sc.nextLine();
-        String stt = sc.nextLine();
-        String[] Stone = stt.split(" ");
-        int[] Stone_Statues = Stream.of(Stone).mapToInt(Integer::parseInt).toArray();
+        String Statue = sc.nextLine();
+        String[] Statue_split = Statue.split(" ");
+        int[] Stone_Statues = Stream.of(Statue_split).mapToInt(Integer::parseInt).toArray();
         Stack<Integer> stack = new Stack<>();
-        int[] L_R_statue_cnt = new int[2];
-        Arrays.fill(L_R_statue_cnt, 0);
+        int[] L_R_cnt = new int[2];
+        Arrays.fill(L_R_cnt, 0);
         int tmp = 0;
-        stack.push(Stone_Statues[0]);
+        int round = 1;
         if (N == 1) {
             System.out.println(1);
         } else {
-
+            stack.push(Stone_Statues[0]);
             for (int i = 1; i < N; i++) {
                 if (Stone_Statues[i] == Stone_Statues[i - 1]) {
                     stack.push(Stone_Statues[i]);
                 } else {
-//                if (stack.size() == 1) {
-//                    stack.clear();
-//                } else {
+                    tmp = 0;
                     tmp = stack.size();
+                    round++;
                     if (stack.contains(1)) {
-                        if (tmp > L_R_statue_cnt[0]) {
-                            L_R_statue_cnt[0] = tmp;
+                        if (tmp > L_R_cnt[0]) {
+                            L_R_cnt[0] = tmp;
                         }
                     } else {
-                        if (tmp > L_R_statue_cnt[1]) {
-                            L_R_statue_cnt[1] = tmp;
+                        if (tmp > L_R_cnt[1]) {
+                            L_R_cnt[1] = tmp;
                         }
                     }
                     stack.clear();
                     stack.push(Stone_Statues[i]);
-//                }
                 }
             }
-            System.out.println(L_R_statue_cnt[0] - L_R_statue_cnt[1]);
+            if (L_R_cnt[0] == 1 & L_R_cnt[1] == 1) {
+                System.out.println(1);
+            } else {
+                if (L_R_cnt[1] == 1) {
+                    System.out.println(L_R_cnt[0]);
+                } else {
+                    System.out.println(L_R_cnt[0] - L_R_cnt[1]);
+                }
+            }
         }
     }
 }
