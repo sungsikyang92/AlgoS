085fdf7e79280910e80eb0e23dc88fa93193ea4d
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-17 19:02:31 +0900

    PG lv.0 JAVA PRAC

---
+++ PROGRAMMERS/JAVA/src/겹치는선분의길이.java
@@ -0,0 +1,17 @@
+public class 겹치는선분의길이 {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] num_cnt = new int[200];
+        for (int i = 0; i < lines.length; i++) {
+            for (int j = lines[i][0] + 100; j < lines[i][1] + 100; j++) {
+                num_cnt[j]++;
+            }
+        }
+        for (int i = 0; i < num_cnt.length; i++) {
+            if (num_cnt[i] > 1) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
