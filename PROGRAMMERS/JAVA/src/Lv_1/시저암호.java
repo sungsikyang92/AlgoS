36e500f9678f464a928fb17faece4d2df60843f6
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-22 21:57:07 +0900

    PG lv.1 JAVA PRAC

---
+++ PROGRAMMERS/JAVA/src/Lv_1/시저암호.java
@@ -0,0 +1,25 @@
+package Lv_1;
+
+public class 시저암호 {
+    public static String solution(String s, int n) {
+        String answer = "";
+        char[] s_chr = s.toCharArray();
+        for (int i = 0; i < s_chr.length; i++) {
+            if (Character.isLetter(s_chr[i])) {
+                if (Character.isUpperCase(s_chr[i])) {
+                    answer += (char) ((s_chr[i] - 'A' + n) % 26 + 'A');
+                } else if (Character.isLowerCase(s_chr[i])) {
+                    answer += (char) ((s_chr[i] - 'a' + n) % 26 + 'a');
+                }
+            } else {
+                answer += s_chr[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String result = solution("AB", 1);
+        System.out.println(result);
+    }
+}
---
+++ PROGRAMMERS/JAVA/src/Lv_1/최소직사각형.java
@@ -0,0 +1,18 @@
+package Lv_1;
+
+import java.util.Arrays;
+
+public class 최소직사각형 {
+    public static int solution(int[][] sizes) {
+        int answer = 0;
+        Arrays.sort(sizes, ());
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] sizes = new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        int result = solution(sizes);
+
+    }
+}
