8161c9247bb821a99b331a0d3b7986a5c1378f2a
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-26 02:13:38 +0900

    PG Lv.1 JAVA PRAC

--- PROGRAMMERS/JAVA/src/Lv_1/푸드파이트대회.java
+++ PROGRAMMERS/JAVA/src/Lv_1/푸드파이트대회.java
@@ -1,15 +1,34 @@
 package Lv_1;
 
-public class 푸드파이트대회 {
-    public String solution(int[] food) {
-        String answer = "";
-        int[] cnt = new int[(food.length - 1) * 2 + 1];
-        for (int i = 0; i < cnt.length; i++) {
-            if (i == cnt.length / 2) {
-                cnt[i] = 0;
-            }
+import java.util.Arrays;
 
+public class 푸드파이트대회 {
+    public static String solution(int[] food) {
+        String answer = "";
+        int[] left = new int[food.length - 1];
+        int[] right;
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] % 2 != 0) {
+                food[i]--;
+            }
+        }
+        for (int i = 1; i <= food.length - 1; i++) {
+            left[i - 1] = food[i] / 2;
+        }
+        right = left;
+        for (int i = 0; i < left.length; i++) {
+            answer += Integer.toString(i + 1).repeat(left[i]);
+        }
+        answer += "0";
+        for (int i = right.length; i > 0; i--) {
+            answer += Integer.toString(i).repeat(right[i - 1]);
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] food = new int[]{1, 3, 4, 6};
+        String result = solution(food);
+        System.out.println(result);
     }
 }
