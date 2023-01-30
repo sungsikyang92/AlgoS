a1ae78db0372db1110538b735e72d710199eeb74
sungsikyang92 <sungsik.yang92@gmail.com>
2023-01-31 01:46:26 +0900

    PG lv.1 JAVA PRAC

--- PROGRAMMERS/JAVA/src/Lv_1/옹알이2.java
+++ PROGRAMMERS/JAVA/src/Lv_1/옹알이2.java
@@ -1,5 +1,40 @@
 package Lv_1;
 
-public class 옹알이2 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class 옹알이2 {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        String[] bab = {"aya", "ye", "woo", "ma"};
+        String[] cant_bab = {"ayaaya", "yeye", "woowoo", "mama"};
+
+        for (String babb : babbling) {
+            for (String cant : cant_bab) {
+                babb = babb.replace(cant, "1");
+            }
+            for (String ba : bab) {
+                babb = babb.replace(ba, "2");
+            }
+            int flag = 0;
+            for (int i = 0; i < babb.length(); i++) {
+                if (!babb.substring(i, i + 1).equals("2")) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+//        String[] babbling = new String[]{"aya", "yee", "u", "maa"};
+        String[] babbling = new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        int result = solution(babbling);
+        System.out.println(result);
+    }
 }
