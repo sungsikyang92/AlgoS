a12be6241ea37cf3c13daed71e28db0e1590bf57
sungsikyang92 <sungsik.yang92@gmail.com>
2023-02-07 23:15:13 +0900

    test

---
+++ PROGRAMMERS/JAVA/src/Lv_2/캐시.java
@@ -0,0 +1,16 @@
+package Lv_2;
+
+public class 캐시 {
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int cacheSize = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int result = solution(cacheSize, cities);
+        System.out.println("결과는" + result);
+    }
+}
