87b8e2c883df8f4aee32a271fc62bd01553d6a6c
sungsikyang92 <sungsik.yang92@gmail.com>
2023-02-08 23:54:11 +0900

    PG Lv.2 JAVA PRAC

--- PROGRAMMERS/JAVA/src/Lv_2/캐시.java
+++ PROGRAMMERS/JAVA/src/Lv_2/캐시.java
@@ -1,9 +1,27 @@
 package Lv_2;
 
+import java.util.LinkedList;
+
 public class 캐시 {
     public static int solution(int cacheSize, String[] cities) {
         int answer = 0;
-
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        LinkedList<String> cache = new LinkedList<>();
+        for (int i = 0; i < cities.length; i++) {
+            String s = cities[i].toUpperCase();
+            if (cache.remove(s)) {
+                answer++;
+                cache.add(s);
+            } else {
+                answer += 5;
+                if (cache.size() >= cacheSize) {
+                    cache.remove(0);
+                }
+                cache.add(s);
+            }
+        }
         return answer;
     }
 
