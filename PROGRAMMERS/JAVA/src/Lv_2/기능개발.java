ea54c6cc67fc1a15f8b29938ed8ff2314f08738f
sungsikyang92 <sungsik.yang92@gmail.com>
2023-02-17 01:14:02 +0900

    PG Lv.2 JAVA 기능개발

--- PROGRAMMERS/JAVA/src/Lv_2/기능개발.java
+++ PROGRAMMERS/JAVA/src/Lv_2/기능개발.java
@@ -1,12 +1,13 @@
 package Lv_2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class 기능개발 {
-    public static List<Integer> solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
+    public static int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> tmp = new ArrayList<>();
         LinkedList<Integer> queue_prog = new LinkedList<>();
         LinkedList<Integer> queue_spd = new LinkedList<>();
         for (int i = 0; i < progresses.length; i++) {
@@ -14,32 +15,37 @@ public class 기능개발 {
             queue_spd.add(speeds[i]);
         }
         int cnt = 0;
-        while (true) {
-            if (queue_prog.isEmpty()) {
-                break;
-            } else {
-                for (int i = 0; i < queue_prog.size(); i++) {
-                    queue_prog.set(i, queue_prog.get(i) + queue_spd.get(i));
-
-                }
-                System.out.println(queue_prog);
-                for (int i = 0; i < queue_prog.size(); i++) {
-                    if (queue_prog.get(i) == 100) {
-                        cnt++;
-                    } else {
-                        break;
-                    }
-                }
-                System.out.println(cnt);
-                answer.add(cnt);
-                System.out.println(answer);
-                for (int i = 0; i < cnt; i++) {
-                    queue_prog.poll();
-                    queue_spd.poll();
-                }
-                cnt = 0;
+        while (!queue_prog.isEmpty()) {
+            for (int i = 0; i < queue_prog.size(); i++) {
+                queue_prog.set(i, queue_prog.get(i) + queue_spd.get(i));
             }
-
+            for (int i = 0; i < queue_prog.size(); i++) {
+                if (queue_prog.get(i) >=  100) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            tmp.add(cnt);
+            for (int i = 0; i < cnt; i++) {
+                queue_prog.remove();
+                queue_spd.remove();
+            }
+            cnt = 0;
+        }
+        int n = 0;
+        for (int i = 0; i < tmp.size(); i++) {
+            if (tmp.get(i) != 0) {
+                n++;
+            }
+        }
+        int[] answer = new int[n];
+        int idx = 0;
+        for (int i = 0; i < tmp.size(); i++) {
+            if (tmp.get(i) != 0) {
+                answer[idx] = tmp.get(i);
+                idx++;
+            }
         }
         return answer;
     }
@@ -48,7 +54,7 @@ public class 기능개발 {
         int[] progresses = {93,30,55};
         int[] speeds = {1,30,5};
 
-        List<Integer> result = solution(progresses, speeds);
-        System.out.println(result);
+        int[] result = solution(progresses, speeds);
+        System.out.println(Arrays.toString(result));
     }
 }
