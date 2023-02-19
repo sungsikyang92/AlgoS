70129a2ef162ffacbd44c9556ba75aa911e18a2d
sungsikyang92 <sungsik.yang92@gmail.com>
2023-02-19 23:06:06 +0900

    PG Lv.2 JAVA PRINTER

--- PROGRAMMERS/JAVA/src/Lv_2/lv2test.java
+++ PROGRAMMERS/JAVA/src/Lv_2/lv2test.java
@@ -1,76 +1,51 @@
 package Lv_2;
 
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.*;
 
 public class lv2test {
-    public static boolean chking(LinkedList<Character> queue) {
-        Stack<String> stk = new Stack<>();
-        if (queue.getFirst() == ']' || queue.getFirst() == '}' || queue.getFirst() == ')') {
-            return false;
-        } else if (queue.getLast() == '[' || queue.getLast() == '{' || queue.getLast() == '(') {
-            return false;
-        } else {
-            for (int i = 0; i < queue.size(); i++) {
-                if (queue.get(i) == '[') {
-                    stk.add("[");
-                } else if (queue.get(i) == '{') {
-                    stk.add("{");
-                } else if (queue.get(i) == '(') {
-                    stk.add("(");
-                } else if (queue.get(i) == ']') {
-                    if (stk.isEmpty()) {
-                        return false;
-                    } else if (stk.peek() != "[") return false;
-                    stk.pop();
-                } else if (queue.get(i) == '}') {
-                    if (stk.isEmpty()) {
-                        return false;
-                    }else if (stk.peek() != "{") {
-                        return false;
+    public static int solution(int[] priorities, int location) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> result_idx = new ArrayList<>();
+        LinkedList<Integer> pq = new LinkedList<>();
+        LinkedList<Integer> idx = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+            idx.add(i);
+        }
+        int max_num = Integer.MIN_VALUE;
+        for (int num : pq) {
+            if (num > max_num) {
+                max_num = num;
+            }
+        }
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < pq.size(); i++) {
+                if (pq.peek() < max_num) {
+                    pq.add(pq.poll());
+                    idx.add(idx.poll());
+                } else if (pq.peek() == max_num) {
+                    result.add(pq.poll());
+                    result_idx.add(idx.poll());
+                    if (pq.isEmpty()) {
+                        break;
+                    } else {
+                        max_num = Integer.MIN_VALUE;
+                        for (int num : pq) {
+                            if (num > max_num) {
+                                max_num = num;
+                            }
+                        }
                     }
-                    stk.pop();
-                } else if (queue.get(i) == ')') {
-                    if (stk.isEmpty()) {
-                        return false;
-                    }else if (stk.peek() != "(") {
-                        return false;
-                    }
-                    stk.pop();
                 }
             }
         }
-        if (!stk.isEmpty()) {
-            return false;
-        } else {
-            return true;
-        }
+        return result_idx.indexOf(location) + 1;
     }
-    public static int solution(String s) {
-        int answer = 0;
-        if (s.length() % 2 == 1) {
-            return 0;
-        }
-        LinkedList<Character> queue = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            queue.add(s.charAt(i));
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (chking(queue)) {
-                answer++;
-            }
-            char first = queue.getFirst();
-            queue.poll();
-            queue.add(first);
-        }
-        return answer;
-    }
-
 
     public static void main(String[] args) {
-        String s = "[](){}";
-//        String s = "}]()[{";
-        int result = solution(s);
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
+        int result = solution(priorities, location);
         System.out.println(result);
     }
 }
