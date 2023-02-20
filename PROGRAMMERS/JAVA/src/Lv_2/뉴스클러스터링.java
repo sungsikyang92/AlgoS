093cd05b437f237d77c4dc3ba62bf0a836c20cd5
sungsikyang92 <sungsik.yang92@gmail.com>
2023-02-21 01:28:08 +0900

    PG Lv.2 JAVA NEWS CLUSTERING

--- PROGRAMMERS/JAVA/src/Lv_2/뉴스클러스터링.java
+++ PROGRAMMERS/JAVA/src/Lv_2/뉴스클러스터링.java
@@ -1,6 +1,7 @@
 package Lv_2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,51 +10,52 @@ public class 뉴스클러스터링 {
         int answer = 0;
         String a = str1.toUpperCase();
         String b = str2.toUpperCase();
-        if (a.length() % 2 == 0) {
-            a = a + " ";
-        }
-        if (b.length() % 2 == 0) {
-            b = b + " ";
-        }
+
         List<String> a_str = new ArrayList<>();
         List<String> b_str = new ArrayList<>();
         List<String> union = new ArrayList<>();
-        HashMap<String, Integer> intersec = new HashMap<>();
+        List<String> intersection = new ArrayList<>();
 
-        for (int i = 0; i < a.length(); i++) {
+        for (int i = 0; i < a.length() - 1; i++) {
             String tmp_a = a.substring(i, i + 2);
             if (tmp_a.matches("[a-zA-Z]+")) {
                 a_str.add(tmp_a);
-                if (!union.contains(tmp_a)) {
-                    union.add(tmp_a);
-                }
-                intersec.put(tmp_a, intersec.getOrDefault(tmp_a, 0) + 1);
             }
         }
-        System.out.println(a_str);
-        for (int i = 0; i < b.length(); i+=2) {
+        for (int i = 0; i < b.length() - 1; i++) {
             String tmp_b = b.substring(i, i + 2);
             if (tmp_b.matches("[a-zA-Z]+")) {
                 b_str.add(tmp_b);
-                if (!union.contains(tmp_b)) {
-                    union.add(tmp_b);
-                }
-                intersec.put(tmp_b, intersec.getOrDefault(tmp_b, 0) + 1);
             }
         }
-
-        System.out.println(a_str);
-        System.out.println(b_str);
-        System.out.println(union);
-        System.out.println(intersec);
+        for (String astr : a_str) {
+            if (b_str.remove(astr)) {
+                intersection.add(astr);
+            }
+            union.add(astr);
+        }
+        for (String bstr : b_str) {
+            union.add(bstr);
+        }
+        double jaccard = 0;
+        if (union.size() == 0) {
+            jaccard = 1;
+        } else {
+            jaccard = (double) intersection.size() / union.size();
+        }
+        answer = (int) (jaccard * 65536);
         return answer;
     }
 
     public static void main(String[] args) {
-        String str1 = "FRANCE";
-        String str2 = "french";
+//        String str1 = "FRANCE";
+//        String str2 = "french";
 //        String str1 = "E=M*C^2";
 //        String str2 = "e=m*c^2";
+        String str1 = "aa1+aa2";
+        String str2 = "AAAA12";
+//        String str1 = "A+C";
+//        String str2 = "DEF";
         int result = solution(str1, str2);
         System.out.println("결과는 = " + result);
     }
