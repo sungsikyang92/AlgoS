3f21be72f64476f2fe1b0705fa2ed1d323fa6080
sungsikyang92 <sungsik.yang92@gmail.com>
2023-03-05 18:08:54 +0900

    PG Lv.2 JAVA MORE SPICY

--- PROGRAMMERS/JAVA/src/PG/Lv_2/더맵게.java
+++ PROGRAMMERS/JAVA/src/PG/Lv_2/더맵게.java
@@ -1,4 +1,30 @@
 package PG.Lv_2;
 
+import java.util.*;
+
 public class 더맵게 {
+    public static int solution(int[] scoville, int k) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int sc : scoville) {
+            pq.add(sc);
+        }
+        while (pq.peek() < k) {
+            if (pq.size() == 1) {
+                return -1;
+            }
+            pq.add(pq.poll() + pq.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 8, 10, 12};
+        int k = 7;
+        int result = solution(scoville, k);
+        System.out.println("결과는 = " + result);
+    }
 }
